d22d2c0f139abc638bb4f1dd409a203c81b9fac2
Vignesh21J <vignesh11212003@gmail.com>
2026-01-02 11:44:10 +0530

    feat: implemented the core idea that is Room View and Creation in Admin panel

--- base/views.py
+++ base/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Room
 
 # Create your views here.
 def Home(request):
-    # return HttpResponse('Hello User!')
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {
+        'rooms':rooms
+    }
+
+    return render(request, 'home.html', context)
+
+def GetRoom(request, pk):
+    room = get_object_or_404(Room, id=pk)
+    context = {
+        'room':room
+    }
+    return render(request, 'base/room.html', context)
