95d61c745c52546a52ae70f92be4271d2ceafaf0
Vignesh21J <vignesh11212003@gmail.com>
2026-01-02 17:14:30 +0530

    feat: basic auth done

---
+++ users/forms.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import get_user_model
+User = get_user_model()
+from django.contrib.auth.forms import UserCreationForm
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class RegisterUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields = ["username", "email", "password1", "password2"]
+        labels = {
+            "username": "Username",
+            "email": "Email address",
+        }
+        widgets = {
+            "username": forms.TextInput(attrs={
+                "placeholder": "Enter your username"
+            }),
+            "email": forms.EmailInput(attrs={
+                "placeholder": "Enter your email address"
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["password1"].widget.attrs["placeholder"] = "Enter password"
+        self.fields["password2"].widget.attrs["placeholder"] = "Confirm password"
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        return email.strip().lower()
+    
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={
+            "placeholder": "Enter your email address"
+        })
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["password"].widget.attrs["placeholder"] = "Enter password"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django.contrib.auth import login, logout
+
+from django.contrib import messages
+
+from .forms import RegisterUserForm
+
+from .forms import LoginUserForm
+
+from django.views.decorators.http import require_POST
 
 # Create your views here.
+def RegisterView(request):
+    
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Account created Successfully..!")
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+    else:
+        form = RegisterUserForm()
+
+    context = {
+        'form':form
+    }
+    return render(request, 'users/register.html', context)
+
+def LoginView(request):
+    if request.method == "POST":
+        form = LoginUserForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, "Logged In Successfully!")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid email or password")
+    else:
+        form = LoginUserForm()
+
+    return render(request, "users/login.html", {"form": form})
+
+
+@require_POST
+def LogoutView(request):
+    logout(request)
+    messages.success(request, "Logged out successfully.")
+    return redirect('login')
